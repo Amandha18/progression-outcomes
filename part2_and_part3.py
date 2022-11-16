58eff9400d7d0bf1ce6524ec15878d646c453b33
Amandha18 <86169120+Amandha18@users.noreply.github.com>
2022-11-16 11:00:14 +0530

    Update part2_and_part3.py

--- part2_and_part3.py
+++ part2_and_part3.py
@@ -1,11 +1,14 @@
-# saves the outcomes in a list
-# saves the outcomes in a text file and open it in notepad
+# saves the outcomes in a text file 
+# display file data in terminal
+# option to open it in notepad
+
 import webbrowser
 
-progression_outcome_list = []
-progression_credit_list = []
+# color codes 
+CRED = "\033[91m" #red
+CEND = "\033[0m"  #white
 
-def display_data_in_terminal():
+def display_data_in_terminal(progression_outcome_list,progression_credit_list):
     title = "  A L L   P R O G R E S S I O N  O U T C O M E S - S T A F F   V E R S I O N \n"
     print("\n-------------------------------------------------------------------------------------------------------")
     print(title)
@@ -14,10 +17,11 @@ def display_data_in_terminal():
     for i in range(len(progression_outcome_list)):
         print(progression_outcome_list[i].ljust(50), progression_credit_list[i])
 
-def save_in_text_file():
+def save_in_text_file(progression_outcome_list,progression_credit_list):
     file_path = "text_file_cw.txt"
     file_data = open(file_path, "w")
     
+    # writes all the data into a txt file
     file_data.write("PROGRESSION OUTCOMES AND CREDITS LIST\n")
     result = "\n".join("{} \t-\t {}".format(x, y) for x, y in zip(progression_outcome_list, progression_credit_list))
     file_data.write(result)
@@ -25,8 +29,17 @@ def save_in_text_file():
 
     print("Data saved in text file!!!\n\tfile path: ",file_path)
 
-    open_file = input("\nEnter 'Y' to view all the data in text file : ").lower()
-    if open_file == "y":
+    # reads the file and disaply data in terminal
+    print()
+    file_data = open(file_path,"r")
+    print(file_data.read())
+    file_data.close()
+
+    # opens file as a text file/notepad
+    open_file = input("\nEnter 'Y'(or any key) to view all the data in a text file or 'Q' to exit program : ").lower()
+    if open_file != "q":
         webbrowser.open(file_path)
     else:
-        exit()
+        print(CRED + "END OF PROGRAM file ..." + CEND)
+        
+    
