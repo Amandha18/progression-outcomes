6aeb749360e51fb84eb34ab5a5573682ac996269
Amandha18 <86169120+Amandha18@users.noreply.github.com>
2022-11-17 11:21:18 +0530

    Update part1_student_version.py

--- part1_student_version.py
+++ part1_student_version.py
@@ -3,19 +3,56 @@
 
 from part1_staff_version import staff_main
 from part4_dictionary import dictionary_program_main
-from tabulate import tabulate # used to draw the border in the title
 
 # color codes 
 CRED = "\033[91m" #red
 CEND = "\033[0m"  #white
 
-def validate_credits(credits):
+# add border around the title
+def add_title_border(text):
+    # unicodes for box drawing 
+    # double line
+    # https://en.wikipedia.org/wiki/Box-drawing_character#Unicode
+
+    horizontal = "\u2550"
+    top_left_corner ="\u2554"
+    top_right_corner = "\u2557"
+    vertical = "\u2551"
+    bottom_right_corner ="\u255D"
+    bottom_left_corner = "\u255A"
+
+    print(top_left_corner+horizontal*(len(text)+10)+top_right_corner)
+    print(vertical+f"     {text}     "+vertical)
+    print(bottom_left_corner+horizontal*(len(text)+10)+bottom_right_corner)
+
+# box the progression outcome of the student
+def add_outcome_border(text):
+    # unicodes for box drawing 
+    # single line
+    # https://en.wikipedia.org/wiki/Box-drawing_character#Unicode
+
+    horizontal = "\u2500"
+    top_left_corner ="\u250C"
+    top_right_corner = "\u2510"
+    vertical = "\u2502"
+    bottom_left_corner ="\u2514"
+    bottom_right_corner = "\u2518"
+    sectioning_down = "\u252C"
+    sectioning_up = "\u2534"
+
+    print(top_left_corner+horizontal*26+sectioning_down+horizontal*(len(text)+5)+top_right_corner)
+    print(vertical+"Your Progression Outcome: "+vertical+f" {text}    "+vertical)
+    print(bottom_left_corner+horizontal*26+sectioning_up+horizontal*(len(text)+5)+bottom_right_corner)
+
+# validates whether credits are lower or equal than 120 and divisible by 20
+def validate_credits(credits): 
     if credits > 120 or credits < 0 or credits % 20 != 0:
         print("Out of range")
     else:
         return True
 
-def progression_outcome(pass_credits,fail_credits):
+# returns the progression outcome based on pass credits and fail credits
+def progression_outcome(pass_credits,fail_credits): 
     if pass_credits == 120:
         outcome = "Progress"
     elif pass_credits == 100:
@@ -30,16 +67,16 @@ def progression_outcome(pass_credits,fail_credits):
 def student_main():
     
     title = "S T U D E N T    V E R S I O N"
-    print(tabulate([[title]],tablefmt="fancy_grid"))
+    add_title_border(title)
     print()
     
     while True:
         try:
             pass_credits = int(input("Please enter your credits at pass: "))
             if validate_credits(pass_credits) is True:
-                if pass_credits == 120:
+                if pass_credits == 120:  # if credits add upto 120, it returns the outcome
                     outcome = progression_outcome(pass_credits,0)
-                else:
+                else: # if credits does not add upto 120, ask the user for other credits
                     defer_credits = int(input("Please enter your credits at defer: "))
                     if validate_credits(defer_credits) is True:
                         if pass_credits + defer_credits == 120:
@@ -62,23 +99,26 @@ def student_main():
             else:
                 continue
 
-        except ValueError:
+        except ValueError: # user input is not an integer
             print("Integer Required")
             continue
 
         else:
-            print(tabulate([["Your Progression Outcome: ",outcome]],tablefmt="simple_outline"))
-            print("\n\n")
+            add_outcome_border(outcome)
+            print()
             break
+    print(CRED + "END OF STUDENT VERSION. RETURNING TO MAIN MENU.." + CEND)
+    
 
 if __name__ =='__main__':
 
     title = "P R O G R E S S I O N   O U T C O M E  - C O U R S E W O R K"
-    print(tabulate([[title]],tablefmt="heavy_grid"))
+    add_title_border(title)
     print()
 
+    # main menu with 4 options
     while True:
-        version_number = input("\nChoose Option from the Menu\n\t1.Student Version\n\t2.Staff Version\n\t3.Staff Version with Dictionary\n\t4.Exit the Program\nEnter Option: ")
+        version_number = input("\nChoose an option from the Menu\n\t1.Student Version\n\t2.Staff Version\n\t3.Staff Version with Dictionary\n\t4.Exit the Program\nEnter Option: ")
         print()
         if version_number == "1":
             student_main()
@@ -87,16 +127,9 @@ if __name__ =='__main__':
         elif version_number == "3":
             dictionary_program_main() 
         elif version_number == "4":
-            print("Exiting program")
+            print(CRED + "Option 4 entered. \nEXITING PROGRAM. THANK YOU!" + CEND)
             exit()
         else:
             print("Input not Recognized")
         
-        
-
-
-""" REFERENCES
-tabulate tables/tabling/border - 
-applying a delay/sleep timer -
-string manipulation documentation -  
-"""
+       
