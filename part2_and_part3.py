8460c89a290451512a095eed0453269fe83da329
Amandha Panagoda <86169120+Amandha18@users.noreply.github.com>
2022-11-29 13:11:02 +0530

    Update part2_and_part3.py

--- part2_and_part3.py
+++ part2_and_part3.py
@@ -1,34 +1,37 @@
 # saves the outcomes in a text file 
 # display file data in terminal
-# option to open it in notepad
-
-import webbrowser
 
 # color codes 
 CRED = "\033[91m" #red
 CEND = "\033[0m"  #white
 
 # part 2
-# displaying listed data in terminal
 def display_data_in_terminal(progression_outcome_list,progression_credit_list):
+    """ Display the listed data on screen
+    parameters -> the 2 lists which store the progression outcomes and their credits in numerical form"""
+
     title = "  A L L   P R O G R E S S I O N  O U T C O M E S - S T A F F   V E R S I O N \n"
     print("\n-------------------------------------------------------------------------------------------------------")
     print(title)
     
     print("Progression Outcome".ljust(52) + "Credits\n")
-    for i in range(len(progression_outcome_list)):
+    list_length = len(progression_outcome_list)
+    for i in range(list_length):
         print(progression_outcome_list[i].ljust(50), progression_credit_list[i])
     print("\n-------------------------------------------------------------------------------------------------------")
     
 # part 3
-# writing and reading data from a file
 def save_in_text_file(progression_outcome_list,progression_credit_list):
+    """ Saves multiple progression outcomes in a file. 
+    Parameters -> list of outcomes and the credit list """
+
     file_path = "text_file_cw.txt"
-    file_data = open(file_path, "w")
+    file_data = open(file_path, "w",encoding="UTF-8")
     
     # writes all the data into a txt file
     file_data.write("PROGRESSION OUTCOMES AND CREDITS LIST - FILE DATA\n")
-    for i in range(len(progression_outcome_list)):
+    list_length = len(progression_outcome_list)
+    for i in range(list_length):
         outcome = progression_outcome_list[i].ljust(50)
         result = f"\n{outcome}  {progression_credit_list[i]}"
         file_data.write(result)
@@ -39,16 +42,9 @@ def save_in_text_file(progression_outcome_list,progression_credit_list):
     # reads the file and display data in terminal
     read_file = input("\nEnter 'Y'(or any key) to read the data in text file or 'Q' to exit program : ").lower()
     if read_file != "q":
-        file_data = open(file_path,"r")
+        file_data = open(file_path,"r",encoding="UTF-8")
         print(file_data.read())
         file_data.close()
 
-        # opens file as a text file/in notepad
-        open_file = input("\nEnter 'Y'(or any key) to view all the data in a text file(notepad) or 'Q' to exit program : ").lower()
-        if open_file != "q":
-            webbrowser.open(file_path)
-            print(CRED + "END OF STAFF VERSION. RETURNING TO MAIN MENU..." + CEND)
-        else:
-            print(CRED + "EXITING STAFF VERSION..." + CEND)
     else:
         print(CRED + "EXITING STAFF VERSION..." + CEND)
