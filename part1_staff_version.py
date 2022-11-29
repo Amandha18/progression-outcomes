ae26a40121496bdf07ceb429f8c5a29d4149bb50
Amandha Panagoda <86169120+Amandha18@users.noreply.github.com>
2022-11-29 13:10:25 +0530

    Update part1_staff_version.py

--- part1_staff_version.py
+++ part1_staff_version.py
@@ -1,4 +1,5 @@
 # staff version validation and outcomes
+# display multiple outcomes
 # saves progression outcomes in a list
 # display horizontal histogram
 
@@ -15,15 +16,16 @@ exclude_count = 0
 progression_outcome_list = []
 progression_credit_list = []
 
-# color codes for exiting program and display progression outcome
+# color codes when exiting program and to display progression outcome
 CRED = "\033[91m" #red
 CBLUE = "\033[34m" #blue
 CEND = "\033[0m"  #white
 
-# function to add borders around title
+
 def add_title_border(text):
-    # unicodes for box drawing 
-    # https://en.wikipedia.org/wiki/Box-drawing_character#Unicode
+    """function to add borders around title
+    unicodes for box drawing
+    https://en.wikipedia.org/wiki/Box-drawing_character#Unicode"""
 
     horizontal = "\u2550"
     top_left_corner ="\u2554"
@@ -36,7 +38,9 @@ def add_title_border(text):
     print(vertical+f"     {text}     "+vertical)
     print(bottom_left_corner+horizontal*(len(text)+10)+bottom_right_corner)
 
-def reset_stored_data(): # resets the previous data if the user decides to re-run the staff version 
+def reset_stored_data(): 
+    """Resets the variables to its initial values"""
+    
     global pass_credits,defer_credits,fail_credits,progress_count,trailer_count,retriever_count,exclude_count,progression_credit_list,progression_outcome_list
     pass_credits = 0
     defer_credits = 0
@@ -48,15 +52,18 @@ def reset_stored_data(): # resets the previous data if the user decides to re-ru
     progression_outcome_list = []
     progression_credit_list = []
 
-# validate whether credits are less than or equal to 120 and divisible by 20
 def validate_credits(credits):
+    """Returns true if the credits are valid, i.e. credits below 120 and divisible by 20"""
+
     if credits > 120 or credits < 0 or credits % 20 != 0:
         print("Out of range\n")
+        return False
     else:
         return True
 
-# returns the progression outcome based on pass credits and fail credits
 def progression_outcome(pass_credits,fail_credits):
+    """Returns the progression outcome based on pass credits and fail credits"""
+
     if pass_credits == 120:
         outcome = "Progress"
     elif pass_credits == 100:
@@ -67,8 +74,9 @@ def progression_outcome(pass_credits,fail_credits):
         outcome = "Do not progress - module retriever"
     return outcome
 
-# Horizontal Histogram for the outcomes
 def horizontal_histogram():
+    """ Print a horizontal histogram based on the progression outcomes """
+
     global progress_count,trailer_count,retriever_count,exclude_count
     progress_count = progression_outcome_list.count("Progress")
     trailer_count = progression_outcome_list.count("Progress (module trailer)")
@@ -85,9 +93,10 @@ def horizontal_histogram():
     print(f"\n{progression_outcome_list.__len__()} outcomes in total")
     print("-------------------------------------------------------------------------------------------------------")
 
-# Extentions of the staff version
-# Menu option to choose to display listed outputs and file handeling
+# Extentionof the staff version
 def menu():
+    """ Menu option to choose to display listed progression outcomes and file handeling"""
+
     global progression_outcome_list,progression_credit_list
     menu_option = input("Enter 'Y'(or any key) to view all the data in terminal and 'Q' to quit the program : ").lower()
     if menu_option != "q":
@@ -98,7 +107,7 @@ def menu():
             save_in_text_file(progression_outcome_list,progression_credit_list) # imported function
         else:
             print(CRED + "'Q' ENTERED. EXITING STAFF VERSION..." + CEND)
-            
+          
     else:
         print(CRED + "'Q' ENTERED. EXITING STAFF VERSION..." + CEND)
         
@@ -112,7 +121,7 @@ def staff_main():
     add_title_border(title)
     print()
     run_program = "y"
-    
+
     while run_program != "q":
         while True:
             try:
@@ -139,7 +148,7 @@ def staff_main():
                                 else:
                                     continue
                         else:
-                            continue 
+                            continue
                 else:
                     continue # program continues to ask user for valid input
 
@@ -162,3 +171,7 @@ def staff_main():
     if run_program == 'q':
         horizontal_histogram() # display horizontal histogram
     menu() # menu to display all the data in terminal, file saving or exit
+
+        
+if __name__ == "__main__":
+    staff_main()
